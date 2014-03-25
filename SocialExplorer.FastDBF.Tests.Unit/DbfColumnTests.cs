eef75a58c1a3e3ef7248544f55f1980914b2e378
Ferdinando Santacroce <ferdinando.santacroce@gmail.com>
2014-03-25 21:10:12 +0100

    Adds test project Enables NuGet package restore

---
+++ SocialExplorer.FastDBF.Tests.Unit/DbfColumnTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using NUnit.Framework;
+
+
+namespace SocialExplorer.FastDBF.Tests.Unit
+{
+	[TestFixture]
+    public class DbfColumnTests
+    {
+		# region Setup and Tear Down
+		/// <summary>
+		/// TestFixtureSetUp called once before any tests have been run in the same TestFixture
+		/// </summary>
+		[TestFixtureSetUp]
+		public void FixtureSetUp()
+		{
+		}
+
+		/// <summary>
+		/// TestFixtureTearDown called once after all tests have been run in the same TestFixture
+		/// </summary>
+		[TestFixtureTearDown]
+		public void FixtureTearDown()
+		{
+		}
+
+		/// <summary>
+		/// SetsUp is called once before each Test within the same TestFxiture
+		/// If this throws an exception no Test in the TestFixture are run.
+		/// </summary>
+		[SetUp]
+		public void SetUp()
+		{
+		}
+
+		/// <summary>
+		/// TearsDown is called once after each Test within the same TestFixture.
+		/// Will not run if no tess are run due to [SetUp] throwing an exception
+		/// </summary>
+		[TearDown]
+		public void TearDown()
+		{
+		}
+		#endregion
+
+
+		#region Tests
+
+		#endregion
+	}
+}
